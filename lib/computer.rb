2b9eb9eec0129181a4b1894e5f58286597356f7d
Matt DeVoe <matthew.devoe@gmail.com>
2017-05-25 08:55:26 -0600

    Final Commit

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,4 +1,5 @@
 require './lib/board'
+require 'pry'
 
 class Computer
 
@@ -8,9 +9,11 @@ class Computer
   def initialize
     @board = Board.new.board
     @previous_placement = ""
+    @random_tries = 0
   end
 
   def run
+    wipe_board
     x_first_placement(random_num)
     x_second_placement(random_num)
     y_first_placement(random_num)
@@ -21,30 +24,37 @@ class Computer
   def x_first_placement(selection_one)
       @board[selection_one[0]][selection_one[1].to_i] = "x1"
   end
-  
+
 
   def x_second_placement(selection_two)
-    previous_ship_placement("x1")
-    if @board[selection_two[0]][selection_two[1].to_i] != ""
-        x_second_placement(random_num)
+      if  @board[selection_two[0]][selection_two[1].to_i] != ""
+          @random_tries += 1
+          check_random_tries
+          x_second_placement(random_num)
     elsif selection_two[0] == @previous_placement[0]
-        if @previous_placement[1].to_i - selection_two[1].to_i == -1
+        if previous_ship_placement("x1")[1].to_i - selection_two[1].to_i == -1
           @board[selection_two[0]][selection_two[1].to_i] = "x2"
-        elsif @previous_placement[1].to_i - selection_two[1].to_i == 1
+        elsif previous_ship_placement("x1")[1].to_i - selection_two[1].to_i == 1
           @board[selection_two[0]][selection_two[1].to_i] = "x2"
         else
+          @random_tries += 1
+          check_random_tries
           x_second_placement(random_num)
         end
-    elsif selection_two[1] == @previous_placement[1]
-      if @previous_placement[0].ord - selection_two[0].ord == -1
-        @board[selection_two[0]][selection_two[1].to_i] = "x2"
-      elsif @previous_placement[0].ord - selection_two[0].ord == 1
-        @board[selection_two[0]][selection_two[1].to_i] = "x2"
+    elsif selection_two[1] == previous_ship_placement("x1")[1]
+      if  previous_ship_placement("x1")[0].ord - selection_two[0].ord == -1
+          @board[selection_two[0]][selection_two[1].to_i] = "x2"
+      elsif previous_ship_placement("x1")[0].ord - selection_two[0].ord == 1
+          @board[selection_two[0]][selection_two[1].to_i] = "x2"
       else
-        x_second_placement(random_num)
+          @random_tries += 1
+          check_random_tries
+          x_second_placement(random_num)
       end
     else
-      x_second_placement(random_num)
+          @random_tries += 1
+          check_random_tries
+          x_second_placement(random_num)
     end
   end
 
@@ -53,58 +63,76 @@ class Computer
     if @board[selection_three[0]][selection_three[1].to_i] == ""
         @board[selection_three[0]][selection_three[1].to_i] = "y1"
     else
+        @random_tries += 1
+        check_random_tries
         y_first_placement(random_num)
     end
   end
 
 
   def y_second_placement(selection_four)
-    if @board[selection_four[0]][selection_four[1].to_i] != ""
-      y_second_placement(random_num)
+    if    @board[selection_four[0]][selection_four[1].to_i] != ""
+          @random_tries += 1
+          check_random_tries
+          y_second_placement(random_num)
     elsif (selection_four[0] == previous_ship_placement("y1")[0])
         if previous_ship_placement("y1")[1].to_i - selection_four[1].to_i == -1
           @board[selection_four[0]][selection_four[1].to_i] = "y2"
         elsif previous_ship_placement("y1")[1].to_i - selection_four[1].to_i == 1
           @board[selection_four[0]][selection_four[1].to_i] = "y2"
         else
+          @random_tries += 1
+          check_random_tries
           y_second_placement(random_num)
         end
     elsif (selection_four[1] == previous_ship_placement("y1")[1])
-      if previous_ship_placement("y1")[0].ord - selection_four[0].ord == -1
-        @board[selection_four[0]][selection_four[1].to_i] = "y2"
+      if  previous_ship_placement("y1")[0].ord - selection_four[0].ord == -1
+          @board[selection_four[0]][selection_four[1].to_i] = "y2"
       elsif previous_ship_placement("y1")[0].ord - selection_four[0].ord == 1
-        @board[selection_four[0]][selection_four[1].to_i] = "y2"
+          @board[selection_four[0]][selection_four[1].to_i] = "y2"
       else
-        y_second_placement(random_num)
+          @random_tries += 1
+          check_random_tries
+          y_second_placement(random_num)
       end
     else
-        y_second_placement(random_num)
+          @random_tries += 1
+          check_random_tries
+          y_second_placement(random_num)
     end
   end
 
 
   def y_third_placement(selection_five)
-    if @board[selection_five[0]][selection_five[1].to_i] != ""
-      y_third_placement(random_num)
-    elsif (selection_five[0] == previous_ship_placement("y2")[0]) &&
-      ((previous_ship_placement("y1"))[0] == selection_five[0])
+    if  @board[selection_five[0]][selection_five[1].to_i] != ""
+        @random_tries += 1
+        check_random_tries
+        y_third_placement(random_num)
+    elsif  (selection_five[0] == previous_ship_placement("y2")[0]) &&
+           ((previous_ship_placement("y1"))[0] == selection_five[0])
         if (previous_ship_placement("y2")[1].to_i - selection_five[1].to_i == -1)
-          @board[selection_five[0]][selection_five[1].to_i] = "y3"
+           @board[selection_five[0]][selection_five[1].to_i] = "y3"
         elsif (previous_ship_placement("y2")[1].to_i - selection_five[1].to_i == 1)
-          @board[selection_five[0]][selection_five[1].to_i] = "y3"
+           @board[selection_five[0]][selection_five[1].to_i] = "y3"
         else
+          @random_tries += 1
+          check_random_tries
           y_third_placement(random_num)
         end
     elsif (selection_five[1] == previous_ship_placement("y2")[1]) &&
         ((previous_ship_placement("y1"))[1] == selection_five[1])
       if (previous_ship_placement("y2")[0].ord - selection_five[0].ord == -1)
-              @board[selection_five[0]][selection_five[1].to_i] = "y3"
+         @board[selection_five[0]][selection_five[1].to_i] = "y3"
       elsif (previous_ship_placement("y2")[0].ord - selection_five[0].ord == 1)
-              @board[selection_five[0]][selection_five[1].to_i] = "y3"
+         @board[selection_five[0]][selection_five[1].to_i] = "y3"
       else
+          @random_tries += 1
+          check_random_tries
           y_third_placement(random_num)
       end
     else
+        @random_tries += 1
+        check_random_tries
         y_third_placement(random_num)
     end
   end
@@ -123,9 +151,29 @@ class Computer
 
   def random_num
     random_number = ""
-    num_gen = (1..4)
+    num_gen = rand(1..4)
     letter_gen = ["a", "b", "c", "d"]
-    random_number = letter_gen.sample + rand(num_gen).to_s
+    # binding.pry
+    random_number = letter_gen.sample + num_gen.to_s
+  end
+
+  def check_random_tries
+    if @random_tries == 100
+      @random_tries = 0
+      run
+    end
+  end
+
+  def wipe_board
+    @board.each do |key, row|
+      row.each do |index|
+      if    (index == "x1") || (index == "x2")
+            index = ""
+      elsif (index == "y1") || (index == "y2") || (index == "y3")
+            index = ""
+        end
+      end
+    end
   end
 
 
