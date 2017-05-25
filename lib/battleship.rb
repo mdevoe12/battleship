15e9dcad87706b830f19ba3c3a43d4d14d5f365c
Matt DeVoe <matthew.devoe@gmail.com>
2017-05-24 22:52:58 -0600

    instructions added to REPL

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -6,38 +6,49 @@ class Battleship
 
   def initialize
     @shot_counter = 0
-
   end
+
+
 
 puts "=========================================="
 puts "Welcome to BATTLESHIP: The Next Generation"
 puts "=========================================="
+puts
+puts "What is your name?"
+name = gets.chomp
+puts "Hello Captain #{name}, I'm Commander Data."
+puts "I'll be your guide during our battle with the Klingons"
+puts
 
 
   def start_sequence
 
-    puts "My name is Commander Data, I'll be your guide during our battle with the Klingons"
     puts
     puts "Would you like to (p)lay, read the (i)instructions or (q)uit?"
     puts
     choice = gets.chomp
 
-    if choice == "i" || choice == "instructions"
-      instructions
+    if    choice == "i" || choice == "instructions"
+          instructions
     elsif choice == "p" || choice == "play"
-      play
+          play
     elsif choice == "q" || choice == "quit"
-      puts "Come back when you're ready to battle the Klingons."
-      end_game
+          quit
     else
-      puts "That's not a valid choice, please choose again."
-      start_sequence
+          puts "That's not a valid choice, please choose again."
+          start_sequence
     end
+  end
 
+  def quit
+    puts
+    puts "Come back when you're ready to battle the Klingons."
+    end_game
   end
 
   def instructions
-    puts "instructions" #insert code for printing instructions to screen
+    puts
+    instructions_text
     start_sequence
   end
 
@@ -60,34 +71,33 @@ puts "=========================================="
   end
 
   def player_check(shot)
-    if ((shot[0].ord > 100) || (shot[0].ord < 97))
-      p "Your selection is off the grid, please choose again."
-      request_shot
+    if    ((shot[0].ord > 100) || (shot[0].ord < 97))
+          p "Your selection is off the grid, please choose again."
+          request_shot
     elsif (shot[1].to_i < 1) || (shot[1].to_i > 4)
-      p "Your selection is off the grid, please choose again."
-      request_shot
+          p "Your selection is off the grid, please choose again."
+          request_shot
     elsif @computer_player.board[shot[0]][shot[1].to_i] == ""
-       @computer_player.board[shot[0]][shot[1].to_i] = "M"
-       puts
-       puts "Your photon torpedo missed!"
-       puts
-       @shot_counter += 1
+          @computer_player.board[shot[0]][shot[1].to_i] = "M"
+          puts
+          puts "Your photon torpedo missed!"
+          puts
+          @shot_counter += 1
     elsif (@computer_player.board[shot[0]][shot[1].to_i].include?("x")) ||
-      (@computer_player.board[shot[0]][shot[1].to_i].include?("y"))
-        @computer_player.board[shot[0]][shot[1].to_i] = "H"
-        puts
-      puts "It's a hit!"
-      puts
-      @shot_counter += 1
+          (@computer_player.board[shot[0]][shot[1].to_i].include?("y"))
+           @computer_player.board[shot[0]][shot[1].to_i] = "H".red
+          puts
+          puts "It's a hit!"
+          puts
+          @shot_counter += 1
     else
-      puts "Not a valid selection, please choose again."
-      request_shot
+          puts "Not a valid selection, please choose again."
+          request_shot
     end
     ship_status(@computer_player)
     puts
     puts "The Klingons are shooting! Brace for impact!!!"
     puts
-
     display_board
     computer_shot
   end
@@ -122,23 +132,22 @@ puts "=========================================="
     request_shot
   end
 
-
   def ship_status(player)
     x_count = 0
     y_count = 0
     player.board.each do |key, row|
       row.each do |index|
-      if (index == "x1") || (index == "x2")
-          x_count += 1
+      if    (index == "x1") || (index == "x2")
+            x_count += 1
       elsif (index == "y1") || (index == "y2") || (index == "y3")
-          y_count += 1
+            y_count += 1
         end
       end
     end
     if (player = @computer_player) && ((x_count == 0) && (y_count == 0))
       puts "================================================================="
       puts "          V-I-C-T-O-R-Y"
-      puts "                                     "
+      puts
       puts "You destroyed the Klingon's Fleet with #{@shot_counter} torpedoes"
       puts "================================================================="
       end_game
@@ -195,6 +204,36 @@ puts "=========================================="
     puts "============================="
   end
 
+  def instructions_text
+    puts "====================================================================="
+    puts "BATTLESHIP: The Next Generation is an intense test simulation for "
+    puts "battling the Klingon threat. If you choose to (p)lay, the Klingons"
+    puts "will automatically place their two-ship fleet on a 4x4 grid."
+    puts "One of their ships will hold two places on the 4x4 grid."
+    puts "The second ship will hold three places on the 4x4 grid."
+    puts
+    puts "After the Klingons place their ships, you will be asked to place"
+    puts "your ships on 4x4 grid. The grid corresponds to a letter and a number"
+    puts "placement. For example, a1 will place the first spot of a ship in the"
+    puts "top left corner of the 4x4 board."
+    puts
+    puts "All ship placements must be placed horizontally or vertically on the"
+    puts "board. If the placecment is invalid, the simulation will tell you to"
+    puts "choose again. After all ships are placed, the battle will begin."
+    puts
+    puts "Once the battle starts, you will asked to fire a proton torpedo at"
+    puts "the Klingon's fleet. To do so, enter a valid coordinate on the 4x4"
+    puts "grid. These coordinates will also conform to a a-d, 1-4 match."
+    puts
+    puts "As you fire shots, the Klingons will as well. You'll see an updated"
+    puts "battle map with your hits, designated with an H or a miss, designated"
+    puts "with an M."
+    puts
+    puts "The battle is over when one of the fleets are destroyed."
+    puts
+    puts "Good luck, Captain."
+    puts "====================================================================="
+  end
 end
 
 
