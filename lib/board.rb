bc59b322a84dfa365b4758a2eca4b0f5285d9bc0
Matthew DeVoe <matthew.devoe@gmail.com>
2021-01-17 15:31:27 -0700

    simple refactor of board

--- lib/board.rb
+++ lib/board.rb
@@ -4,7 +4,18 @@ class Board
   attr_reader :player_side, :computer_side
 
   def initialize
-    @player_side = Side.new.layout
-    @computer_side = Side.new.layout
+    @player_side = layout
+    @computer_side = layout
+  end
+
+  private
+
+  def layout
+    {
+      "a" => [nil, "", "", "", ""],
+      "b" => [nil, "", "", "", ""],
+      "c" => [nil, "", "", "", ""],
+      "d" => [nil, "", "", "", ""]
+    }
   end
 end
